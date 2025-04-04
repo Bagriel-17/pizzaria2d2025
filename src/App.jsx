d4a2109af5f0c387aba3f49538caff7e1e8065c6
Bagriel-17 <rm95012@estudante.fieb.edu.br>
2025-04-04 08:25:51 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import {useState } from "react"
-   
+
 function App(){
 
   const [nome, setNome] = useState('ronaldo')
-
-  return (
-    <div>
-      <h3>PIZZARIA 2D</h3>
-
-      <input 
+  const Formulario = () => {
+    return (
+      <>
+        <h3>Formulário</h3>
+        <input 
         ClassName="nome"
         onChange={(e)=>{setNome(e.target.value)}}
         type="text" />
-
+        
     <button 
       className="botao"
-      onClick={()=>{alert(nome)}}
-
+      onClick={()=>{
+        alert('você é ' + nome)}}
     >
       CLIQUE AQUI
     </button>
+
+      </>
+    )
+  }
+   return (
+    <div>
+      <h3>PIZZARIA 2D</h3>
+      
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      
     </div>
   )
 }
